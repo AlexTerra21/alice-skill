6ba5eaf9608330efc8cb5f5fcae82c795a844e9c
AlexTerra <baewoo21@gmail.com>
2023-12-24 07:41:24 +0300

    Add logger

--- cmd/skill/main.go
+++ cmd/skill/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
+
+	"github.com/AlexTerra21/alice-skill/internal/logger"
+	"go.uber.org/zap"
 )
 
 // функция main вызывается автоматически при запуске приложения
@@ -16,13 +18,17 @@ func main() {
 
 // функция run будет полезна при инициализации зависимостей сервера перед запуском
 func run() error {
-	fmt.Println("Running server on", flagRunAddr)
-	return http.ListenAndServe(flagRunAddr, http.HandlerFunc(webhook))
+	if err := logger.Initialize(flagLogLevel); err != nil {
+		return err
+	}
+	logger.Log.Info("Running server", zap.String("address", flagRunAddr))
+	return http.ListenAndServe(flagRunAddr, logger.RequestLogger(webhook))
 }
 
 // функция webhook — обработчик HTTP-запроса
 func webhook(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
+		logger.Log.Debug("got request with bad method", zap.String("method", r.Method))
 		// разрешаем только POST-запросы
 		w.WriteHeader(http.StatusMethodNotAllowed)
 		return
@@ -39,4 +45,5 @@ func webhook(w http.ResponseWriter, r *http.Request) {
         "version": "1.0"
       }
     `))
+	logger.Log.Debug("sending HTTP 200 response")
 }
